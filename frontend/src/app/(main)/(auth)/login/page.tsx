9fabbb8241253a98b60579f5cecee2eae13384e2
DaniilGordeev <gordeev.daniil.andreevic@gmail.com>
2026-04-08 01:31:39 +0500

    Сделан базовый роутинг

---
+++ frontend/src/app/(main)/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/shared/ui";
+
+export default function LoginPage() {
+  return (
+    <>
+      <h1>This login page</h1>
+      <Button variant={"outline"}>Click</Button>
+    </>
+  )
+}
---
+++ frontend/src/app/(main)/(content)/page.tsx
@@ -0,0 +1,5 @@
+export default function ContentPage() {
+  return (
+    <h1>This content page</h1>
+  )
+}
